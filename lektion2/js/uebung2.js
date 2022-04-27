3da4742b436905ff4bdb759bc1bfc13dc022bc4f
DEADNUB <103416389+DEADNUB@users.noreply.github.com>
2022-04-27 14:08:00 +0200

    test

--- lektion2/js/uebung2.js
+++ lektion2/js/uebung2.js
@@ -15,7 +15,7 @@ function aufgabe1() {
 /**
  * 2) Gebe die Laenge des Arrays aus.
  */
-function aufgabe2() {
+function aufgabe2() {sfasfasfasf
     console.debug("Aufgabe 2 ==========================");
 
     let helden = ["Spider-Man", "Batman", "Superman"]
