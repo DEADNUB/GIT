64477b9fb18d884564f8dac8631071057fd80142
DEADNUB <103416389+DEADNUB@users.noreply.github.com>
2022-04-27 14:41:27 +0200

    Add files via upload

---
+++ uebung1.js
@@ -0,0 +1,63 @@
+/**
+ * Uebung 1: Variablen
+ */
+
+/**
+ * 1) Eine Variable land erstellen
+ */
+//ToDo: füge ab hier Deinen Code ein
+
+let land = "Spanien"
+
+/**
+ * 2) Die Variable ausgeben
+ */
+function aufgabe2() {
+  console.debug("Aufgabe 2 =====================");
+  console.debug("Spanien");
+}
+
+/**
+ * 3) Erstelle die Variablen
+ */
+//ToDo: füge ab hier Deinen Code ein
+
+function aufgabe2() {
+  console.debug("Aufgabe 2 ===================");
+  console.debug("Spanien");
+  console.debug("Aufgabe 4 ===================");
+  console.debug("Berlin", "Mount Everest", "Nil");
+  console.debug("Aufgabe 5 ===================");
+}
+
+
+/**
+ * 4) Gebe den Variablen Werte
+ */
+function aufgabe4() {
+  console.debug("Aufgabe 4 ===================");
+  //ToDo: füge ab hier Deinen Code ein
+
+  console.debug(stadt,"Berlin", berg ,"Mount Everest", fluss,"Nil");
+}
+
+/**
+ * 5) Erstelle die Variablen mit verschiedenem Typ
+ */
+function aufgabe5() {
+  console.debug("Aufgabe 7 ===================");
+
+  //ToDo: füge ab hier Deinen Code ein
+
+  console.debug.("Elon Musk", "50","true");
+}
+
+/**
+ * Hauptfunktion, in der die Aufgaben aufgerufen werden
+ */
+function main() {
+  aufgabe2();
+  aufgabe4();
+  aufgabe5();
+}
+main();
