c4f946be2d974a510ba11c69bcf24a85c28652b4
DEADNUB <103416389+DEADNUB@users.noreply.github.com>
2022-05-09 14:47:20 +0200

    update

---
+++ lektion 4/Übung 4.js
@@ -0,0 +1,24 @@
+function artikel_Gekauft() {
+    let artikelName = "Streukäse"
+    let artikelGekauft = true
+
+    if(artikelgekauft ==true){
+        console.debug("[Artikel]") \"" + "\" wurde gekauft")
+    }
+}
+
+function artikel_Hinzufügen(){
+    gruppenname = "Getreide"
+    let artieklName = "Quinoa"
+    if(artikelNam.length >0) {
+        console.debug("[" + gruppenName + "] \ "" + artikelName + "\" hinzugefügt")
+    }
+}
+
+function gruppe_Hinzufügen(){
+    let gleicheGruppen = []
+    let neueGruppe = "Getränke"
+    if (gleicheGruppen.lenght == 0){
+        console.debug("[App] Gruppe \ "")
+    }
+}
